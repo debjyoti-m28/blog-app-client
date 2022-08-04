4d808f1007c8b1a9d17c5d177308701af73f936f
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-08-04 21:33:07 +0530

    upadte in headerfile

--- src/hearder/Header.jsx
+++ src/hearder/Header.jsx
@@ -25,4 +25,4 @@ export default function Header() {
   )
 }
 
-// "https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+//PreviousHeaderImg: "https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
